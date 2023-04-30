2765bd9b5d692903450ab77837139a5945a49264
AMiro <aj.miro.b@gmail.com>
2023-04-30 13:05:14 -0500

    Add user model and route + test with seed file

---
+++ seeds/seed.js
@@ -0,0 +1,26 @@
+const sequelize = require("../config/connection");
+const { User } = require("../models");
+
+const userData = [
+  {
+    username: "john_doe",
+    password: "password123"
+  },
+  {
+    username: "jane_doe",
+    password: "password456"
+  }
+];
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
